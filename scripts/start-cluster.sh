a66c1a01c7058ab4ec9e9ec0672797250bd5f6bc
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-11 19:44:31 +0200

    status check

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -5,8 +5,8 @@ echo "Lauch Hadoop cluster..."
 
 kubectl apply -f ../k8s/namenode-pvc.yaml
 kubectl apply -f ../k8s/namenode-service.yaml
-kubectl apply -f ../k8s/namenode-deployment.yaml
+kubectl apply -f ../k8s/namenode-deploy.yaml
 kubectl apply -f ../k8s/datanode-service.yaml
-kubectl apply -f ../k8s/datanode-statefulset.ya
+kubectl apply -f ../k8s/datanode-statefulset.yaml
 
 echo "Cluster is launched."
