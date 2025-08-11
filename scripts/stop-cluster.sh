5d85af999e501c174eb0bdf40af750c9383e7cd6
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-11 19:58:39 +0200

    config + typo

--- scripts/stop-cluster.sh
+++ scripts/stop-cluster.sh
@@ -5,7 +5,7 @@ echo "Stopping Hadoop cluster..."
 
 kubectl delete -f ../k8s/datanode-statefulset.yaml
 kubectl delete -f ../k8s/datanode-service.yaml
-kubectl delete -f ../k8s/namenode-deployment.yaml
+kubectl delete -f ../k8s/namenode-deploy.yaml
 kubectl delete -f ../k8s/namenode-service.yaml
 kubectl delete -f ../k8s/namenode-pvc.yaml
 
