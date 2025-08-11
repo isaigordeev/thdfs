6e2663939d92945abb52d331551fbab9cd9f5883
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-12 00:07:15 +0200

    config for master connexion

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Lauch Hadoop cluster..."
 
+kubectl create configmap hadoop-config --from-file=../configs/core-site.xml
 kubectl apply -f ../k8s/namenode-pvc.yaml
 kubectl apply -f ../k8s/namenode-service.yaml
 kubectl apply -f ../k8s/namenode-deploy.yaml
